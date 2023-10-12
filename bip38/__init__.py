7164d86d61f73a8f2de280956b0a122a527cccb9
meherett <meherett@zoho.com>
2023-10-12 20:27:00 +0300

    Bump: Python-BIP38 from `v0.1.1` to `v0.2.0` package

--- bip38/__init__.py
+++ bip38/__init__.py
@@ -23,7 +23,7 @@ from .bip38 import (
 )
 
 __version__, __license__, __author__, __email__, __description__ = (
-    "v0.1.1",
+    "v0.2.0",
     "MIT",
     "Meheret Tesfaye Batu",
     "meherett.batu@gmail.com",
