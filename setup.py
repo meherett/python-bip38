665c6c327cb12ad930f05ad1bea29570015c965d
meherett <meherett.batu@gmail.com>
2023-10-23 23:12:54 +0300

    Drop: pytest-cov package

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
     extras_require={
         "tests": [
             "pytest>=7.4.0,<8",
-            "pytest-cov>=4.1.0,<5",
             "pyyaml>=6.0.1,<7"
         ],
         "docs": [
