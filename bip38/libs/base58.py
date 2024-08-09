cf27ed3ec8f8c4198ed258eca45528fa420441bc
meherett <meherett.batu@gmail.com>
2024-08-09 09:43:59 +0300

    Update: base58 lib implementation

--- bip38/libs/base58.py
+++ bip38/libs/base58.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-from binascii import (
-    hexlify, unhexlify
-)
+from hashlib import sha256
 
-import hashlib
 import six
 
-base58_alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+__base58_alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 
 def string_to_int(data):
@@ -29,73 +26,49 @@ def ensure_string(data):
     return data
 
 
-def encode(data: bytes) -> str:
-    """Encode bytes to a base58-encoded string"""
+def encode(data, alphabet=__base58_alphabet):
+    enc = ""
+    val = string_to_int(data)
+    while val >= len(alphabet):
+        val, mod = divmod(val, len(alphabet))
+        enc = alphabet[mod] + enc
+    if val:
+        enc = alphabet[val] + enc
 
-    # Convert big-endian bytes to integer
-    n = int('0x0' + hexlify(data).decode('utf8'), 16)
+    n = len(data) - len(data.lstrip(b"\0"))
+    return alphabet[0] * n + enc
 
-    # Divide that integer into bas58
-    res = []
-    while n > 0:
-        n, r = divmod(n, 58)
-        res.append(base58_alphabet[r])
-    res = ''.join(res[::-1])
 
-    # Encode leading zeros as base58 zeros
-    import sys
-    czero = b'\x00'
-    if sys.version > '3':
-        # In Python3 indexing a bytes returns numbers, not characters.
-        czero = 0
-    pad = 0
+def check_encode(raw, alphabet=__base58_alphabet):
+    chk = sha256(sha256(raw).digest()).digest()[:4]
+    return encode(raw + chk, alphabet)
+
+
+def decode(data, alphabet=__base58_alphabet):
+    if bytes != str:
+        data = bytes(data, "ascii")
+
+    val = 0
+    prefix = 0
     for c in data:
-        if c == czero:
-            pad += 1
-        else:
-            break
-    return base58_alphabet[0] * pad + res
+        val = (val * len(alphabet)) + alphabet.encode("utf-8").find(c)
+        if val == 0:
+            prefix += 1
+
+    dec = bytearray()
+    while val > 0:
+        val, mod = divmod(val, 256)
+        dec.append(mod)
+
+    dec.extend(bytearray(prefix))
+
+    return bytes(dec[::-1])
 
 
-def check_encode(raw):
-    chk = hashlib.sha256(hashlib.sha256(raw).digest()).digest()[:4]
-    return encode(raw + chk)
-
-
-def decode(data: str) -> bytes:
-    """Decode a base58-encoding string, returning bytes"""
-    if not data:
-        return b''
-
-    # Convert the string to an integer
-    n = 0
-    for c in data:
-        n *= 58
-        if c not in base58_alphabet:
-            raise ValueError('Character %r is not a valid base58 character' % c)
-        digit = base58_alphabet.index(c)
-        n += digit
-
-    # Convert the integer to bytes
-    h = '%x' % n
-    if len(h) % 2:
-        h = '0' + h
-    res = unhexlify(h.encode('utf8'))
-
-    # Add padding back.
-    pad = 0
-    for c in data[:-1]:
-        if c == base58_alphabet[0]:
-            pad += 1
-        else:
-            break
-    return b'\x00' * pad + res
-
-
-def check_decode(enc):
-    dec = decode(enc)
+def check_decode(enc, alphabet=__base58_alphabet):
+    dec = decode(enc, alphabet)
     raw, chk = dec[:-4], dec[-4:]
-    if chk != hashlib.sha256(hashlib.sha256(raw).digest()).digest()[:4]:
+    if chk != sha256(sha256(raw).digest()).digest()[:4]:
         raise ValueError("base58 decoding checksum error")
     else:
         return raw
