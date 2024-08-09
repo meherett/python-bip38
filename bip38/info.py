705e1b45ce39ae422cb7f9c367d34c29802ec434
meherett <meherett.batu@gmail.com>
2024-08-09 10:06:24 +0300

    Add: python-bip38 information

---
+++ bip38/info.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Copyright © 2023-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit
+
+from typing import List
+
+__name__: str = "bip38"
+__version__: str = "v1.0.0"
+__license__: str = "MIT"
+__author__: str = "Meheret Tesfaye Batu"
+__email__: str = "meherett.batu@gmail.com"
+__documentation__: str = "https://bip38.readthedocs.com"
+__description__: str = "Python library for implementation of Bitcoin Improvement Proposal - 0038 / BIP38 protocol."
+__url__: str = "https://github.com/meherett/python-bip38"
+__tracker__: str = f"{__url__}/issues"
+__keywords__: List[str] = [
+    "bip38", "bitcoin", "private-key", "pure-python", "encrypt", "decrypt", "passphrase", "wif", "bip-0038"
+]
