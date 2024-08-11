fb7483c0c0773450a91ded03ca6a972786514424
meherett <meherett.batu@gmail.com>
2024-08-11 22:04:18 +0300

    Update: BIP38 package imports

--- bip38/__init__.py
+++ bip38/__init__.py
@@ -1,55 +1,35 @@
 #!/usr/bin/env python3
 
-# Copyright © 2023, Meheret Tesfaye Batu <meherett.batu@gmail.com>
+# Copyright © 2023-2024, Meheret Tesfaye Batu <meherett.batu@gmail.com>
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit
 
 from typing import List
 
-from .bip38 import (
-    uncompress_public_key,
-    compress_public_key,
-    private_key_to_public_key,
-    private_key_to_wif,
-    wif_to_private_key,
-    get_wif_type,
-    get_wif_checksum,
-    get_wif_network,
-    public_key_to_addresses,
-    bip38_encrypt,
-    bip38_decrypt,
-    intermediate_code,
-    create_new_encrypted_wif,
-    confirm_code
+from .info import (
+    __name__,
+    __version__,
+    __license__,
+    __author__,
+    __email__,
+    __documentation__,
+    __description__,
+    __url__,
+    __tracker__,
+    __keywords__
 )
-
-__version__, __license__, __author__, __email__, __description__ = (
-    "v0.3.0",
-    "MIT",
-    "Meheret Tesfaye Batu",
-    "meherett.batu@gmail.com",
-    "Python library for implementation of Bitcoin Improvement Proposal - 0038 / BIP38 protocol."
-)
+from .bip38 import BIP38
 
 __all__: List[str] = [
-    "uncompress_public_key",
-    "compress_public_key",
-    "private_key_to_public_key",
-    "private_key_to_wif",
-    "wif_to_private_key",
-    "get_wif_type",
-    "get_wif_checksum",
-    "get_wif_network",
-    "public_key_to_addresses",
-    "bip38_encrypt",
-    "bip38_decrypt",
-    "intermediate_code",
-    "create_new_encrypted_wif",
-    "confirm_code",
-
+    "__name__",
     "__version__",
     "__license__",
     "__author__",
     "__email__",
+    "__documentation__",
     "__description__",
+    "__url__",
+    "__tracker__",
+    "__keywords__",
+    "BIP38"
 ]
