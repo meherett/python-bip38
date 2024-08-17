8b9b4603719c31965004f8aeb5ace548520761fc
meherett <meherett.batu@gmail.com>
2024-08-17 12:50:16 +0300

    Add: network on BIP38 methods & update exceptions

--- bip38/bip38.py
+++ bip38/bip38.py
@@ -26,9 +26,6 @@ from .p2pkh_address import P2PKHAddress
 from .crypto import (
     double_sha256, get_checksum
 )
-from .utils import (
-    integer_to_bytes, bytes_to_integer, bytes_to_string, get_bytes
-)
 from .const import (
     N,
     MAGIC_LOT_AND_SEQUENCE,
@@ -43,6 +40,12 @@ from .const import (
     EC_MULTIPLIED_PRIVATE_KEY_PREFIX,
     CONFIRMATION_CODE_PREFIX,
     FLAGS
+)
+from .exceptions import (
+    Error, PassphraseError
+)
+from .utils import (
+    integer_to_bytes, bytes_to_integer, bytes_to_string, get_bytes
 )
 from .wif import (
     private_key_to_wif, wif_to_private_key, get_wif_type
@@ -109,19 +112,19 @@ class BIP38:
 
         owner_salt: bytes = get_bytes(owner_salt) if owner_salt else os.urandom(8)
         if len(owner_salt) not in [4, 8]:
-            raise ValueError(f"Invalid owner salt length (expected: 8 or 16, got: {len(bytes_to_string(owner_salt))})")
+            raise Error(f"Invalid owner salt length (expected: 8 or 16, got: {len(bytes_to_string(owner_salt))})")
         if len(owner_salt) == 4 and (not lot or not sequence):
-            raise ValueError(
+            raise Error(
                 f"Invalid owner salt length for non lot/sequence (expected: 16, got: {len(bytes_to_string(owner_salt))})")
         if (lot and not sequence) or (not lot and sequence):
-            raise ValueError(f"Both lot & sequence are required, got: (lot {lot}) (sequence {sequence})")
+            raise Error(f"Both lot & sequence are required, got: (lot {lot}) (sequence {sequence})")
 
         if lot and sequence:
             lot, sequence = int(lot), int(sequence)
             if not 100000 <= lot <= 999999:
-                raise ValueError(f"Invalid lot, (expected: 100000 <= lot <= 999999, got: {lot})")
+                raise Error(f"Invalid lot, (expected: 100000 <= lot <= 999999, got: {lot})")
             if not 0 <= sequence <= 4095:
-                raise ValueError(f"Invalid lot, (expected: 0 <= sequence <= 4095, got: {sequence})")
+                raise Error(f"Invalid lot, (expected: 0 <= sequence <= 4095, got: {sequence})")
 
             pre_factor: bytes = scrypt.hash(unicodedata.normalize("NFC", passphrase), owner_salt[:4], 16384, 8, 8, 32)
             owner_entropy: bytes = owner_salt[:4] + integer_to_bytes((lot * 4096 + sequence), 4)
@@ -185,7 +188,7 @@ class BIP38:
             )))
             public_key_type: str = "compressed"
         else:
-            raise ValueError("Wrong WIF (Wallet Important Format) type")
+            raise Error("Wrong WIF (Wallet Important Format) type")
 
         address: str = P2PKHAddress.encode(
             public_key=private_key.public_key(),
@@ -247,7 +250,7 @@ class BIP38:
         seed_b: bytes = get_bytes(seed) if seed else os.urandom(24)
         intermediate_decode: bytes = check_decode(intermediate_passphrase)
         if len(intermediate_decode) != 49:
-            raise ValueError(f"Invalid intermediate passphrase length (expected: 49, got: {len(intermediate_decode)})")
+            raise PassphraseError(f"Invalid intermediate passphrase length (expected: 49, got: {len(intermediate_decode)})")
 
         magic: bytes = intermediate_decode[:8]
         owner_entropy: bytes = intermediate_decode[8:16]
@@ -259,7 +262,7 @@ class BIP38:
             elif public_key_type == "compressed":
                 flag: bytes = integer_to_bytes(MAGIC_LOT_AND_SEQUENCE_COMPRESSED_FLAG)
             else:
-                raise ValueError(
+                raise Error(
                     f"Invalid public key type (expected: 'uncompressed' or 'compressed', got: {public_key_type})")
         elif magic == integer_to_bytes(MAGIC_NO_LOT_AND_SEQUENCE):
             if public_key_type == "uncompressed":
@@ -267,17 +270,17 @@ class BIP38:
             elif public_key_type == "compressed":
                 flag: bytes = integer_to_bytes(MAGIC_NO_LOT_AND_SEQUENCE_COMPRESSED_FLAG)
             else:
-                raise ValueError(
+                raise Error(
                     f"Invalid public key type (expected: 'uncompressed' or 'compressed', got: {public_key_type})")
         else:
-            raise ValueError(
+            raise Error(
                 f"Invalid magic (expected: {bytes_to_string(integer_to_bytes(MAGIC_LOT_AND_SEQUENCE))}/"
                 f"{bytes_to_string(integer_to_bytes(MAGIC_NO_LOT_AND_SEQUENCE))}, got: {bytes_to_string(magic)})"
             )
 
         factor_b: bytes = double_sha256(seed_b)
         if not 0 < bytes_to_integer(factor_b) < N:
-            raise ValueError("Invalid EC encrypted WIF (Wallet Important Format)")
+            raise Error("Invalid EC encrypted WIF (Wallet Important Format)")
 
         public_key: PublicKey = PublicKey.from_point(
             PublicKey.from_bytes(pass_point).point() * bytes_to_integer(factor_b)
@@ -315,10 +318,10 @@ class BIP38:
             bytes_to_integer(point_b[17:]) ^ bytes_to_integer(derived_half_2)
         ))
         encrypted_point_b: bytes = (
-                point_b_prefix + point_b_half_1 + point_b_half_2
+            point_b_prefix + point_b_half_1 + point_b_half_2
         )
         confirmation_code: str = ensure_string(check_encode((
-                integer_to_bytes(CONFIRMATION_CODE_PREFIX) + flag + address_hash + owner_entropy + encrypted_point_b
+            integer_to_bytes(CONFIRMATION_CODE_PREFIX) + flag + address_hash + owner_entropy + encrypted_point_b
         )))
 
         return dict(
@@ -363,12 +366,12 @@ class BIP38:
 
         confirmation_code_decode: bytes = check_decode(confirmation_code)
         if len(confirmation_code_decode) != 51:
-            raise ValueError(f"Invalid confirmation code length (expected: 102, got: {len(confirmation_code_decode)})")
+            raise Error(f"Invalid confirmation code length (expected: 102, got: {len(confirmation_code_decode)})")
 
         prefix_length: int = len(integer_to_bytes(CONFIRMATION_CODE_PREFIX))
         prefix_got: bytes = confirmation_code_decode[:prefix_length]
         if integer_to_bytes(CONFIRMATION_CODE_PREFIX) != prefix_got:
-            raise ValueError(f"Invalid confirmation code prefix (expected: {prefix_length}, got: {prefix_got})")
+            raise Error(f"Invalid confirmation code prefix (expected: {prefix_length}, got: {prefix_got})")
 
         flag: bytes = confirmation_code_decode[5:6]
         address_hash: bytes = confirmation_code_decode[6:10]
@@ -386,7 +389,7 @@ class BIP38:
         if lot_and_sequence:
             pass_factor: bytes = double_sha256(pass_factor + owner_entropy)
         if bytes_to_integer(pass_factor) == 0 or bytes_to_integer(pass_factor) >= N:
-            raise ValueError("Invalid EC encrypted WIF (Wallet Important Format)")
+            raise Error("Invalid EC encrypted WIF (Wallet Important Format)")
 
         pass_point: bytes = PrivateKey.from_bytes(pass_factor).public_key().raw_compressed()
         salt: bytes = address_hash + owner_entropy
@@ -433,7 +436,7 @@ class BIP38:
                     sequence=sequence
                 )
             return address
-        raise ValueError("Incorrect passphrase or password")
+        raise PassphraseError("Incorrect passphrase or password")
 
     def decrypt(
         self, encrypted_wif: str, passphrase: str, network: Optional[str] = None, detail: bool = False
@@ -471,7 +474,7 @@ class BIP38:
         )
         encrypted_wif_decode: bytes = decode(encrypted_wif)
         if len(encrypted_wif_decode) != 43:
-            raise ValueError(f"Invalid encrypted WIF length (expected: 43, got: {len(encrypted_wif_decode)})")
+            raise Error(f"Invalid encrypted WIF length (expected: 43, got: {len(encrypted_wif_decode)})")
 
         prefix: bytes = encrypted_wif_decode[:2]
         flag: bytes = encrypted_wif_decode[2:3]
@@ -486,7 +489,7 @@ class BIP38:
                 wif_type: Literal["wif", "wif-compressed"] = "wif-compressed"
                 public_key_type: str = "compressed"
             else:
-                raise ValueError(
+                raise Error(
                     f"Invalid flag (expected: {bytes_to_string(integer_to_bytes(NO_EC_MULTIPLIED_WIF_FLAG))} or "
                     f"{bytes_to_string(integer_to_bytes(NO_EC_MULTIPLIED_WIF_COMPRESSED_FLAG))}, got: {bytes_to_string(flag)})"
                 )
@@ -504,7 +507,7 @@ class BIP38:
                 bytes_to_integer(decrypted_half_1 + decrypted_half_2) ^ bytes_to_integer(derived_half_1)
             )
             if bytes_to_integer(private_key) == 0 or bytes_to_integer(private_key) >= N:
-                raise ValueError("Invalid Non-EC encrypted WIF (Wallet Important Format)")
+                raise Error("Invalid Non-EC encrypted WIF (Wallet Important Format)")
 
             public_key: PublicKey = PrivateKey.from_bytes(private_key).public_key()
             address: str = P2PKHAddress.encode(
@@ -513,7 +516,7 @@ class BIP38:
                 public_key_type=public_key_type
             )
             if get_checksum(get_bytes(address, unhexlify=False)) != address_hash:
-                raise ValueError("Incorrect passphrase or password")
+                raise PassphraseError("Incorrect passphrase or password")
 
             wif: str = private_key_to_wif(
                 private_key=private_key, wif_type=wif_type, cryptocurrency=self.cryptocurrency, network=network
@@ -548,7 +551,7 @@ class BIP38:
             if lot_and_sequence:
                 pass_factor: bytes = double_sha256(pass_factor + owner_entropy)
             if bytes_to_integer(pass_factor) == 0 or bytes_to_integer(pass_factor) >= N:
-                raise ValueError("Invalid EC encrypted WIF (Wallet Important Format)")
+                raise Error("Invalid EC encrypted WIF (Wallet Important Format)")
 
             pre_public_key: PublicKey = PrivateKey.from_bytes(pass_factor).public_key()
             salt = address_hash + owner_entropy
@@ -570,7 +573,7 @@ class BIP38:
 
             factor_b: bytes = double_sha256(seed_b)
             if bytes_to_integer(factor_b) == 0 or bytes_to_integer(factor_b) >= N:
-                raise ValueError("Invalid EC encrypted WIF (Wallet Important Format)")
+                raise Error("Invalid EC encrypted WIF (Wallet Important Format)")
 
             # multiply private key
             private_key: bytes = integer_to_bytes(
@@ -609,9 +612,9 @@ class BIP38:
                         sequence=sequence
                     )
                 return wif
-            raise ValueError("Incorrect passphrase or password")
+            raise PassphraseError("Incorrect passphrase or password")
         else:
-            raise ValueError(
+            raise Error(
                 f"Invalid prefix (expected: {bytes_to_string(integer_to_bytes(NO_EC_MULTIPLIED_PRIVATE_KEY_PREFIX))} or "
                 f"{bytes_to_string(integer_to_bytes(EC_MULTIPLIED_PRIVATE_KEY_PREFIX))}, got: {bytes_to_string(prefix)})"
             )
